ce89d5f372bc16f32bfc9170ec555aaebb8972e7
Francesco Pisanu <frapisanu19910gmail.com>
2026-08-10 12:33:57 +0200

    new activity B

---
+++ researchers_night/activity_B/sw.js
@@ -0,0 +1,47 @@
+var NOME_CACHE = "attivita-b-v1";
+var FILE_DA_SALVARE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./images/gondwana.png"
+];
+
+self.addEventListener("install", function (evento) {
+  evento.waitUntil(
+    caches.open(NOME_CACHE).then(function (cache) {
+      return cache.addAll(FILE_DA_SALVARE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (evento) {
+  evento.waitUntil(
+    caches.keys().then(function (nomiCache) {
+      return Promise.all(
+        nomiCache
+          .filter(function (nome) { return nome !== NOME_CACHE; })
+          .map(function (nome) { return caches.delete(nome); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (evento) {
+  evento.respondWith(
+    caches.match(evento.request).then(function (rispostaCache) {
+      if (rispostaCache) {
+        return rispostaCache;
+      }
+      return fetch(evento.request).then(function (rispostaRete) {
+        return caches.open(NOME_CACHE).then(function (cache) {
+          cache.put(evento.request, rispostaRete.clone());
+          return rispostaRete;
+        });
+      });
+    }).catch(function () {
+      return caches.match("./index.html");
+    })
+  );
+});
